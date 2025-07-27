13e1582488b2b8865869b2d125392e6d346e652c
donjet <donjet@DESKTOP-BC0ABO4>
2025-07-27 19:40:13 +0200

    fix: add full backend API URL with HTTPS protocol in production environment
    Updated environment.prod.ts to include full URL (with https://) for apiUrl to ensure correct API endpoint is called.

--- client/auction-ui/src/environments/environment.prod.ts
+++ client/auction-ui/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'auction-ahhth4akffgnenhd.northeurope-01.azurewebsites.net/api'
+  apiUrl: 'https://auction-ahhth4akffgnenhd.northeurope-01.azurewebsites.net/api'
 };
